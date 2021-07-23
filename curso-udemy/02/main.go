2388633701c1d14817b060036fdc4b6b0a58cd7b
ioedumac <eacevedof@yahoo.es>
2021-07-23 19:29:07 +0200

    cap2

--- curso-udemy/02/main.go
+++ curso-udemy/02/main.go
@@ -11,8 +11,9 @@ func main() {
 	//por eso println acepta cualquier tipo como parametro
 
 	// := operador corto de declaraciones cortas, solo se puede usar en el cuerpo de la func no fuera
+	// := inicia la variable, si se desea reasignar hay que usar = 
 	x := 42
 	y := 2 + 4
-
+	x = 5
 	fmt.Println(x, y)
 }
