3684c34d76af063c51ed1483c5537261cce72ec0
ioedumac <eacevedof@yahoo.es>
2021-07-23 19:15:45 +0200

    hola mundo ok

--- curso-udemy/01/main.go
+++ curso-udemy/01/main.go
@@ -6,6 +6,8 @@ import (
 )
 
 func main() {
+	//func x(...any) numero variable de parametros
+
 	nb, err := fmt.Println("hola mundo")
 
 	//blank identifer: le indica al compilador que deseche lo que retorna, forma incorrecta asignacion := (no new variables on left side of :=), correcta =
