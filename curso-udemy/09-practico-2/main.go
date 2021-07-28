db165c4ef73d2e2ada599d9ccbaae1451b35dbf4
ioedumac <eacevedof@yahoo.es>
2021-07-28 18:17:47 +0200

    println ejercicio 9

---
+++ curso-udemy/09-practico-2/main.go
@@ -0,0 +1,17 @@
+//curso practico #2
+//caracteres especiales
+// \a alert, \b backpsace \f form feed \n line feed \r carriage return \v vertical tab \t ...
+package main
+
+import "fmt"
+
+var x int
+var y string
+var z bool
+
+func main() {
+	fmt.Println("hola \vhola-2")
+
+	fmt.Printf("x:%v,y:%v,z:%v\n",x,y,z)
+}
+
