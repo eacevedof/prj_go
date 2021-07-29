4f98c3ed425f7cb5ce7eb786ac02addf022d39a4
ioedumac <eacevedof@yahoo.es>
2021-07-29 21:07:50 +0200

    swithc feo

--- curso-udemy/41-ciclo-for-como-while/main.go
+++ curso-udemy/41-ciclo-for-como-while/main.go
@@ -5,9 +5,14 @@ import (
 )
 
 func main() {
-	i := 0
-	for i < 10 {
-		fmt.Println(i)
-		i++
+	switch {
+	case 2==4:
+		fmt.Println("no deberia imprimir")
+	case 3==3:
+		fmt.Println("deberia imprimir")
+	case 4==5:
+		fmt.Println("no deberia")		
+
 	}
 }
+
