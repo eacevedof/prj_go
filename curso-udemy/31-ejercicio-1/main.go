1f2fdddfdf7ebbfdea1af3df5623a78897b53f2c
ioedumac <eacevedof@yahoo.es>
2021-07-29 20:38:13 +0200

    num en formatos

---
+++ curso-udemy/31-ejercicio-1/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	a:= 42
+	fmt.Printf("digito\tbinario\thexa\n")
+	fmt.Printf("%d\t%b\t%x\n",a,a,a)
+}
+
+/*
+imprime:
+digito  binario hexa
+42      101010  2a
+*/
