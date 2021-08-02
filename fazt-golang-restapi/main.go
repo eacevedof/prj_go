e963424170efa8a288fb7ef51630454619cbc1bc
ioedumac <eacevedof@yahoo.es>
2021-08-02 19:10:25 +0200

    go restapi

--- fazt-golang-restapi/main.go
+++ fazt-golang-restapi/main.go
@@ -4,6 +4,14 @@ import (
 	"fmt"
 )
 
+type Task struct {
+	id      int    `json:id`
+	name    string `json:name`
+	content string `json:content`
+}
+
+type alltasks []Task
+
 func main() {
 	fmt.Println("Hello world")
 }
