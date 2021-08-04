b817dfea4e430753f11bed6d4bd4800509dfa9b0
ioedumac <eacevedof@yahoo.es>
2021-08-04 21:06:14 +0200

    fecth products

--- requests/main.go
+++ requests/main.go
@@ -1,30 +1,11 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
+    "fmt"
 	"request"
-
-	"github.com/gorilla/mux"
 )
 
 func main() {
-	host := "localhost"
-	port := "3001"
-
-	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/", request.Index)
-
-	// TASKS
-	router.HandleFunc("/tasks", request.SelectAll).Methods("GET")
-	router.HandleFunc("/tasks", request.Insert).Methods("POST")
-	router.HandleFunc("/tasks/{id}", request.SelectOne).Methods("GET")
-	router.HandleFunc("/tasks/{id}", request.DeleteOne).Methods("DELETE")
-	router.HandleFunc("/tasks/{id}", request.Update).Methods("PUT")
-
-
-	//con esto salta el warning de conexiones entrantes
-	//log.Fatal(http.ListenAndServe("0.0.0.0:8000", router))
-	log.Fatal(http.ListenAndServe(fmt.Sprintf("%v:%v", host, port), router))
+    strproducts := request.FetchProducts()
+    fmt.Println(strproducts)
 }
--- requests/request/request.go
+++ requests/request/request.go
@@ -1,16 +1,25 @@
 package request
 
 import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"net/http"
-	"strconv"
-
-	"github.com/gorilla/mux"
+   "io/ioutil"
+   "log"
+   "net/http"
 )
 
+func FetchProducts() string {
+    resp, err := http.Get("https://json.theframework.es/index.php?getfile=app_product.json")
+    if err != nil {
+       log.Fatalln(err)
+    }
 
-func Fetch() {
+    //We Read the response body on the line below.
+    body, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+      log.Fatalln(err)
+    }
 
+    //Convert the body to type string
+    sb := string(body)
+    log.Printf(sb)
+    return sb
 }
