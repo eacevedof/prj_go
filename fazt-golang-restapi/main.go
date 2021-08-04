87ea545106c6692bd1d538d18c316319b21da187
ioedumac <eacevedof@yahoo.es>
2021-08-04 18:37:57 +0200

    crud funciona todo

--- fazt-golang-restapi/main.go
+++ fazt-golang-restapi/main.go
@@ -4,30 +4,25 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-
 	"crud"
 
 	"github.com/gorilla/mux"
 )
 
-
-
 func main() {
 	host := "localhost"
 	port := "3001"
-
 
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", crud.Index)
 
 	// TASKS
-	/*
-	router.HandleFunc("/tasks", select_all).Methods("GET")
-	router.HandleFunc("/tasks", insert).Methods("POST")
-	router.HandleFunc("/tasks/{id}", select_one).Methods("GET")
-	router.HandleFunc("/tasks/{id}", delete_one).Methods("DELETE")
-	router.HandleFunc("/tasks/{id}", update).Methods("PUT")
-*/
+	router.HandleFunc("/tasks", crud.Select_all).Methods("GET")
+	router.HandleFunc("/tasks", crud.Insert).Methods("POST")
+	router.HandleFunc("/tasks/{id}", crud.Select_one).Methods("GET")
+	router.HandleFunc("/tasks/{id}", crud.Delete_one).Methods("DELETE")
+	router.HandleFunc("/tasks/{id}", crud.Update).Methods("PUT")
+
 
 	//con esto salta el warning de conexiones entrantes
 	//log.Fatal(http.ListenAndServe("0.0.0.0:8000", router))
